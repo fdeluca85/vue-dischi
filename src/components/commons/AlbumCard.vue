532db89446dcaabd433ba2c2ad5a8a128005a491
francesca <fradeluca85@gmail.com>
2021-12-16 18:01:22 +0100

    album list

---
+++ src/components/commons/AlbumCard.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="album">
+    <img src="" alt="">
+    <h2>Title</h2>
+    <h3>Author</h3>
+    <h3>Year</h3>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name: 'AlbumCard'
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/sections/AlbumList.vue
+++ src/components/sections/AlbumList.vue
@@ -1,10 +1,21 @@
 <template>
+<div class="container">
+    <div class="row">
+        <div class="col">
+           <AlbumCard />
+        </div>
+    </div>
+</div>
   
 </template>
 
 <script>
+import AlbumCard from "../commons/AlbumCard.vue"
 export default {
-
+    name: 'AlbumList',
+    components:{
+        AlbumCard
+    }
 }
 </script>
 
