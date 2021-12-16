80f5abce8abe8872c5110e73350a955fc868d396
francesca <fradeluca85@gmail.com>
2021-12-16 18:46:13 +0100

    logica

--- src/components/commons/AlbumCard.vue
+++ src/components/commons/AlbumCard.vue
@@ -1,17 +1,19 @@
 <template>
 <div class="album">
-    <img src="" alt="">
-    <h2>Title</h2>
-    <h3>Author</h3>
-    <h3>Genre</h3>
-    <h3>Year</h3>
+    <img :src="info.poster" alt="">
+    <h2>{{info.title}}</h2>
+    <h3>{{info.author}}</h3>
+    <h3>{{info.year}}</h3>
 </div>
   
 </template>
 
 <script>
 export default {
-    name: 'AlbumCard'
+    name: 'AlbumCard',
+    props: {
+        info: Object
+    }
 
 }
 </script>
--- src/components/sections/AlbumList.vue
+++ src/components/sections/AlbumList.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="container">
     <div class="row">
-        <div class="col">
-           <AlbumCard />
+        <div class="col" v-for="(album, index) in albums" :key="index">
+           <AlbumCard :info="album"/>
         </div>
     </div>
 </div>
@@ -16,7 +16,26 @@ export default {
     name: 'AlbumList',
     components:{
         AlbumCard
-    }
+    },
+    data() {
+        return{
+            albums: null
+        }
+    },
+    created() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((response) => {
+            // handle success
+            this.albums = response.data.response
+            console.log(response.data.response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+
+        }
+
 }
 </script>
 
