51eae18e85b2ae9e23762f748eb1db3fb43a8202
francesca <fradeluca85@gmail.com>
2021-12-16 16:04:22 +0100

    style header e main

---
+++ src/components/macro/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="" alt="">
+
+        </div>
+    </header>
+  
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang='scss' scoped>
+    header{
+        background-color: #2E3A46;
+        height: 100px;
+    }
+
+
+</style>
---
+++ src/components/macro/Main.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+
+    </main>
+  
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style lang='scss' scoped>
+    main{
+        background-color: #1E2D3B;
+        height: 100vh;
+    }
+
+
+</style>
